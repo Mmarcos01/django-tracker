c3f2bd5a90cb60687c4771057e41c1e77bd636e0
Mmarcos01 <marie.marcos25@gmail.com>
2021-11-20 14:11:45 -0800

    update: models, views

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from .models import Track
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # Create your views here.
+
+
+class TrackerListView(ListView):
+    template_name = 'track-list.html'
+    model = Track
+    context_object_name = 'list_tracker'
+
+class TrackerDetailView(DetailView):
+    template_name = 'track-detail.html'
+    model = Track
+
+class TrackerCreateView(CreateView):
+    template_name = 'track-create.html'
+    model = Track
+    fields = ['title', 'description', 'purchaser']
+    success_url = reverse_lazy('track_list')
+
+class TrackerUpdateView(UpdateView):
+    template_name = 'track-update.html'
+    model = Track
+    fields = ['title', 'description', 'purchaser']
+
+class TrackerDeleteView(DeleteView):
+    template_name = 'track-delete.html'
+    model = Track
+    success_url = reverse_lazy('track_list')
