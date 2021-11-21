e966992d56d4df9038697919cdc40c0569e69ab8
Mmarcos01 <marie.marcos25@gmail.com>
2021-11-20 17:10:53 -0800

    upd: static root

--- tracker_project/settings.py
+++ tracker_project/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../tracker/static/css')
+STATIC_ROOT = os.path.join(BASE_DIR, '../tracker/static/css/tracker.css')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
