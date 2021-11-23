e4b2200b4c76032cadff5d39bacb7c5a9b1c3190
Mmarcos01 <marie.marcos25@gmail.com>
2021-11-22 18:38:13 -0800

    environ

--- tracker_project/settings.py
+++ tracker_project/settings.py
@@ -12,22 +12,27 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env(
+	DEBUG=(bool, False)
+)
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$_+1&k^%ppipjo)%o3$i9ly(89t%9x!_^t=-1%4(r@-72+4r8'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'intense-cove-57839.herokuapp.com', 'https://intense-cove-57839.herokuapp.com/']
-
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
 
 # Application definition
 
